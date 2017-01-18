cf24883ac987737dcc1e2af2bc1eb8546f726601
Jonathan Ly <jonathanlyhv@gmail.com>
2017-01-19 00:32:58 +1100

    React: Added 404 error page.. such wow

---
+++ react/src/components/Shared/NotFound.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const styles = {
+  img: { height: '50vh' }
+}
+
+const NotFound = (props) => {
+  return (
+    <div>
+      <h2 style={{fontFamily: 'Comic Sans MS'}}>
+        <strong>Page cannot be found</strong>
+      </h2>
+      <img src={require('../../images/doge.jpg')} style={styles.img}/>
+      <h3 style={{fontFamily: 'Comic Sans MS', color: '#00ff00'}}>
+        Wow how skill
+      </h3>
+      <h3 style={{fontFamily: 'Comic Sans MS', color: '#ff00ff'}}>
+        Such programming
+      </h3>
+      <h3 style={{fontFamily: 'Comic Sans MS', color: '#ff0000'}}>
+        Much intelligent
+      </h3>
+      <h3>\_(ツ)_/¯</h3>
+    </div>
+  )
+}
+
+
+export default NotFound;
