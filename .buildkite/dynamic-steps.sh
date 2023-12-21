72c837d634123f888a74cae0e4529382976c0da0
Jonathan Ly <jonathan@buildkite.com>
2023-12-21 18:26:30 +1100

    change notification yaml

--- .buildkite/dynamic-steps.sh
+++ .buildkite/dynamic-steps.sh
@@ -3,10 +3,10 @@
 set -eu
 
 echo "  - label: 'Soft fail with custom gh commit status'"
-echo "    command: \"sleep 5 && make test\""
+echo "    command: \"sleep 1 && make test\""
 echo "    soft_fail: true"
 echo "    notify:"
-echo "    - email: \"example@buildkite.com\""
+echo "    - slack: \"#some-channel\""
 # echo "      if: build.state == 'failed'"
 echo "    - github_commit_status:"
 echo "        context: \"Custom commit status test\""
